ce2ecb764ddaff1019dfe0f3e081b26247d67cfe
Alexander.Iskritsky@urfu.me <Alexander.Iskritsky@urfu.me>
2020-10-01 21:07:54 +0300

    Генерация точек, окна поиска и вывод

--- math-modeling/Form1.Designer.cs
+++ math-modeling/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Picture);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.start);
+            this.splitContainer1.Size = new System.Drawing.Size(914, 515);
+            this.splitContainer1.SplitterDistance = 671;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Picture
+            // 
+            this.Picture.BackColor = System.Drawing.SystemColors.Control;
+            this.Picture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Picture.Location = new System.Drawing.Point(0, 0);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(671, 515);
+            this.Picture.TabIndex = 0;
+            this.Picture.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(23, 23);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(91, 35);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.Run);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 515);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button start;
     }
 }
 
--- math-modeling/Form1.cs
+++ math-modeling/Form1.cs
@@ -16,5 +16,59 @@ namespace math_modeling
         {
             InitializeComponent();
         }
+
+        private void Run(object sender, EventArgs e)
+        {
+            int pwidth = Picture.Width;
+            int pheight = Picture.Height;
+            int pixelCount = pheight * pwidth;
+
+            Random random = new Random();
+
+            int pointsCount = random.Next((int)(pixelCount * 0.01), (int)(pixelCount * 0.05));
+            var points = getRandomPoints(pointsCount, pwidth, pheight);
+            var searchWindow = getSearchWindowPoints(pwidth, pheight);
+
+            Bitmap bmp = new Bitmap(pwidth,pheight);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.Black);
+            foreach (var point in points) 
+            {
+                bmp.SetPixel(point.X, point.Y, Color.Red);
+            }
+
+            Pen whitePen = new Pen(Color.White);
+            g.DrawRectangle(whitePen, searchWindow);
+
+            Picture.Image = bmp;
+        }
+
+        public Point[] getRandomPoints(int pointsCount, int maxX, int maxY) 
+        {
+            Random random = new Random();
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < pointsCount; i++)
+            {
+                int x = random.Next(0, maxX);
+                int y = random.Next(0, maxY);
+
+                Point point = new Point() { X = x, Y = y };
+                points.Add(point);
+            }
+
+            return points.ToArray();
+        }
+
+        public Rectangle getSearchWindowPoints(int width, int height) 
+        {
+            Random random = new Random();
+            int wWidth = random.Next((int)(width * 0.2), (int)(width * 0.4));
+            int wHeight = random.Next((int)(height * 0.2), (int)(height * 0.4));
+
+            int x1 = random.Next(width-wWidth);
+            int y1 = random.Next(height - wHeight);
+
+            return new Rectangle(x1, y1, wWidth, wHeight);
+        }
     }
 }
